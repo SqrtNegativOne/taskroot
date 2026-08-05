4d7f82017025af3750275068b4af7fcc99b46c0d
SqrtNegativOne <sqrtnegativ1@gmail.com>
2026-08-05 15:54:57 +0530

    Create DocsScreen.tsx

---
+++ src/screens/docs/DocsScreen.tsx
@@ -0,0 +1,17 @@
+
+export function DocsScreen() {
+    return (
+        <>
+            <div
+                className="main"
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <h2 style={{ color: "var(--fg)" }}>Docs Placeholder</h2>
+            </div>
+        </>
+    );
+}

2783647b97bbcec3d0c3ec7ae30c240fdb92dba2
SqrtNegativOne <sqrtnegativ1@gmail.com>
2026-08-06 22:04:20 +0530

    Update more-screens-dropdown.tsx

--- src/components/shell/more-screens-dropdown.tsx
+++ src/components/shell/more-screens-dropdown.tsx
@@ -37,7 +37,7 @@ export function MoreScreensDropdown() {
             </button>
             {dropdownOpen && (
                 <div className="dropdown-menu">
-                    {["wrap", "graph", "stats", "recap", "dev"].map((screen) => (
+                    {["wrap", "graph", "stats", "recap", "docs", "dev"].map((screen) => (
                         <button
                             key={screen}
                             className="dd-item"
